a18aa97895720bedff45e5ac974fe8f77e5617c1
hugobacher <huba02001@utb.vaxjo.se>
2019-09-06 09:49:16 +0200

    La till program

---
+++ Inlämningsuppgift1/Inlämningsuppgift1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int randomTal = generator.Next(1, 100);
+            while (tal != randomTal)
+            {
+                Console.WriteLine("Skriv ett tal mellan 1 till 100: ");
+                int tal = int.Parse(Console.ReadLine());
+                if (tal > randomTal)
+                    Console.WriteLine("Det gissade talet är för högt");
+                if (tal < randomTal)
+                    Console.WriteLine("Det gissade talet är för lågt");
+            }
+
+
+
+        }
+    }
+}

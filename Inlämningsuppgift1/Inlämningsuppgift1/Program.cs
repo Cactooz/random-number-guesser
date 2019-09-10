2ca8de19c9f72dca3d9fa39da4d37271ae0b8f42
a <a>
2019-09-10 12:57:36 +0200

    tries istället för i och förkortning av koden

--- Inlämningsuppgift1/Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Inlämningsuppgift1/Program.cs
@@ -8,20 +8,18 @@ namespace Inlämningsuppgift1
         {
             Random generator = new Random();
             int randomNumber = generator.Next(1, 100);
-            int number = 0;
-            int i = 0;
+            int number = 0, tries = 0;
             while (number != randomNumber)
             {
                 Console.WriteLine("Skriv ett tal mellan 1 till 100: ");
                 number = int.Parse(Console.ReadLine());
-                i++;
+                tries++;
                 if (number > randomNumber)
-                    Console.WriteLine($"{number} är för högt");
+                    Console.WriteLine($"{number} är för högt!");
                 else if (number < randomNumber)
-                    Console.WriteLine($"{number} är för lågt");
-                else
-                    Console.WriteLine($"Du gissade rätt, talet var {randomNumber}. Det tog {i} försök!");
+                    Console.WriteLine($"{number} är för lågt!");
             }
+            Console.WriteLine($"Du gissade rätt, talet var {randomNumber}. Det tog {tries} försök!");
         }
     }
 }
